ad4b4da34d82c16f7c0880116bdb650911324954
User <User@N1>
2024-09-20 10:29:05 +0300

    Add project files.

---
+++ Eecomerce/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Eecomerce.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
