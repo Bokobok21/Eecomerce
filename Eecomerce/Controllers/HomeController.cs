87208a9652d8b8427b153b102c04323ff24d7f1d
Bokobok21 <bbozhanov06@gmail.com>
2024-09-24 11:12:53 +0300

    new page - helloworld

--- Eecomerce/Controllers/HomeController.cs
+++ Eecomerce/Controllers/HomeController.cs
@@ -22,6 +22,10 @@ namespace Eecomerce.Controllers
         {
             return View();
         }
+        public IActionResult HelloWorld()
+        {
+            return View();
+        }
 
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
